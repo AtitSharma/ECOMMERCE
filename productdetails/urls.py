2f9d12e916dc552b379450d1b64f38453cf9189e
Atit Sharma <atit694161sharma@gmail.com>
2023-05-04 16:46:22 +0545

    added more files of e-commerce

--- productdetails/urls.py
+++ productdetails/urls.py
@@ -1,12 +1,17 @@
 
 from django.urls import path
-from productdetails.views import home,product,product_details,add_to_cart,my_cart
+from productdetails.views import( home,product,
+                                product_details,add_to_cart,my_cart
+                                ,buy_product,place_a_order)
 app_name="product"
 urlpatterns = [ 
     path("",home,name="home"),
     path("product/<int:pk>/",product,name="products"),
     path("product-detail/<int:pk>/",product_details,name="product_detail"),
     path("add_to_cart/<int:pk>/",add_to_cart,name="add_to_cart"),
-    path("my-cart/<str:username>/",my_cart,name="my_cart")
+    path("my-cart/<str:username>/",my_cart,name="my_cart"),
+    path("buy-product/<int:pk>/",buy_product,name="buy_product"),
+    path("place_order/<int:pk>/<int:quantity>/",place_a_order,name="place_order")
+
 
 ]
--- productdetails/views.py
+++ productdetails/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,reverse
 from productdetails.models import Category,Product,Cart
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 
 
@@ -20,7 +22,8 @@ def home(request):
 
 
 def product(request,pk):
-    products=Product.objects.filter(category__id=pk,status="in_stock")
+    products=Product.objects.filter(category__id=pk,status="in_stock",available__gte=1)
+
     context={
         "products":products
     }
@@ -32,7 +35,6 @@ def product_details(request,pk):
     context={
         'products':products
     }
-    print(products)
 
     return render(request,"product_details.html",context)
 
@@ -48,21 +50,58 @@ def add_to_cart(request,pk,quantity=1):
         cart_item.quantity +=quantity
         cart_item.total_price += total_price
         cart_item.save()
-        return redirect('product:home')
+        return HttpResponseRedirect(reverse("product:my_cart",args=(request.user.username,)))
     else:
          Cart.objects.create(username=username,product=product,
                             quantity=quantity,total_price=total_price,
                             address=address)
-    return redirect('product:home')
+    return HttpResponseRedirect(reverse("product:my_cart",args=(request.user.username,)))
 
 
 @login_required
 def my_cart(request,username):
     carts=Cart.objects.filter(username=username)
     context={
-        "carts":carts
+        "carts":carts,
     }
     return render (request,"my_cart.html",context)
+
+def buy_product(request,pk):
+    quantity=request.POST.get("quantity")
+    quantity=int(quantity)
+    available_quantity=Cart.objects.get(pk=pk).product.available
+    if quantity<1:
+        messages.add_message(request,messages.ERROR,"Please choose atleaset 1 quantity")
+        return redirect("product:home")
+    elif quantity > available_quantity:
+        messages.add_message(request,messages.ERROR,"Please choose quantity less and equal to  available")
+        return redirect("product:home")
+    else:
+
+        # remaining_quantity=int(available_quantity)-int(quantity)
+        # print(remaining_quantity)
+        demo=Cart.objects.get(pk=pk).product
+        # demo.available=remaining_quantity
+        price=demo.price
+        # demo.save()
+        Cart.objects.filter(pk=pk).update(quantity=quantity,total_price=price*quantity)
+        carts=Cart.objects.filter(pk=pk)
+        print(carts)
+        
+    return render (request,"buy_product.html",{"carts":carts,"quantity":quantity})
+
+
+
+def place_a_order(request,pk,quantity):
+    available_quantity=Cart.objects.get(pk=pk).product.available
+    remaining_quantity=int(available_quantity)-int(quantity)
+    demo=Cart.objects.get(pk=pk).product
+    demo.available=remaining_quantity
+    demo.save()
+    messages.add_message(request,messages.INFO,"sucessfully bought we will contact you ")
+    Cart.objects.filter(pk=pk).delete()
+    return redirect("product:home")
+
 
 
  
